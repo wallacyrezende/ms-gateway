11c6e21e5f782aea040edffd888eaa6442901310
Wallacy <wallacyrezende@gmail.com>
2022-04-27 19:25:47 -0300

    [MAIN] - Commit initial

---
+++ src/main/java/com/dev/finance/apigatewayzuul/ApiGatewayZuulApplication.java
@@ -0,0 +1,15 @@
+package com.dev.finance.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayZuulApplication.class, args);
+    }
+
+}
